d1448d1c7a1735717babb13b310f79cc3fa09fe3
Aitor García <aitor.garcia@gmail.com>
2010-04-18 13:36:07 +0200

    Added meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
 
   filter_parameter_logging :password
   
+  meta  :title        => "lospresus.de - Las cuentas claras y el chocolate espeso -  Presupuesto municipales de España para humanos",
+        :description  => "lospresus.de es un sitio web con el que queremos analizar los ingresos y gastos municipales de todos los municipios de España basándonos en sus presupuestos oficiales. Estos presupuestos oficiales los obtenemos del Ministerio de Política Territorial.",
+        :keywords     => "presupuestos, ayuntamientos, gastos, ingresos, subvenciones, municipios, municipal, dinero público, gasto público"
+        
+        
   #rescue_from ActionView::TemplateError do render :template => 'errors/404' end
   #rescue_from ActionController::RoutingError do render :template => 'errors/404' end
   
--- app/controllers/municipios_controller.rb
+++ app/controllers/municipios_controller.rb
@@ -3,25 +3,35 @@ class MunicipiosController < ApplicationController
   before_filter :year_context, :only => [:show, :compare]
 
   def show
-    #meta :title => "Lospresus.de"
-    respond_to do |wants|
-      wants.html 
-      wants.xml  { render :xml => @muni }
-    end
+    meta  :title        => "Presupuesto de #{@muni.nombre} en #{@year} - lospresus.de",
+          :description  => "El presupuesto de #{@muni.nombre} (#{@muni.province_name}) para el año #{@year}. Fuente oficial Ministerio de Política Territorial.",
+          :keywords     => "presupuestos, ayuntamientos, gastos, ingresos, subvenciones, municipios, municipal, dinero público, gasto público, #{@muni.nombre}, #{@muni.province_name}"
+    
   end
   
   def search
     q = params[:q]
     @results = Municipio.search(q)
+
+    meta  :title        => "Resultados de la búsqueda '#{q}' - lospresus.de",
+          :description  => "Resultados de la búsqueda '#{q}' en lospresus.de",
+          :keywords     => "presupuestos, ayuntamientos, gastos, ingresos, subvenciones, municipios, municipal, dinero público, gasto público"
+
     render :layout => false
   end  
   
   def compare
     @muni_b = Municipio.find(params[:b_id])
+    meta  :title        => "Comparativa de los municipios de #{@muni.nombre} y #{@muni_b.nombre} - lospresus.de",
+          :description  => "Presupuesto comparado de #{@muni.nombre} (#{@muni.province_name}) y #{@muni_b.nombre} (#{@muni_b.province_name}) en #{@year}. Fuente oficial Ministerio de Política Territorial.",
+          :keywords     => "presupuestos, ayuntamientos, gastos, ingresos, subvenciones, municipios, municipal, dinero público, gasto público, #{@muni.nombre}, #{@muni.province_name}, #{@muni_b.nombre}, #{@muni_b.province_name}"
+
   end
   
   def evolution
-    
+    meta  :title        => "Evolución de los presupuestos de #{@muni.nombre} - lospresus.de",
+          :description  => "Evolución presupuestaria de #{@muni.nombre} (#{@muni.province_name}) a lo largo de los años asi como la evolución del número de sus habitantes. Fuente oficial Ministerio de Política Territorial e INE.",
+          :keywords     => "presupuestos, ayuntamientos, gastos, ingresos, subvenciones, municipios, municipal, dinero público, gasto público, evolución,evolución presupuestaria, población, #{@muni.nombre}, #{@muni.province_name}"
   end
 
   private
