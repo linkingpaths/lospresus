31f6ed71f21b60b649fde4daecc4a1e116b73853
Aitor García <aitor.garcia@gmail.com>
2010-04-18 18:06:25 +0200

    Avanti with tutti le changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,6 +21,29 @@ $(document).ready(function () {
   }).result(function(e,item){
     location.href = item.url;
   });
+
+  $('#city_compare_search').autocomplete("/search", {
+    dataType: "json",
+    parse: function(data) {
+      return $.map(data, function(row) {
+        return {
+          data: row,
+          value: row.name,
+          result: row.name
+        }
+      });
+    },
+    formatItem: function(item) {
+      return "<a href='"+item.url+"'><span>"+item.name+"</span></a>"
+    },
+    minChars: 2,
+    resultsClass: "suggestedOptions",
+    width: "23em",
+    scroll: false,
+    selectFirst: false
+  }).result(function(e,item){
+    location.href = item.url;
+  });
   
   if ($('#holder').length != 0) {single_graph()}
   if ($('#holder_a').length != 0 || $('#holder_b').length != 0) {compare_graph()}
