1f6a99cd270ba2bf39112a676e7d3c611c47ef37
Roberto Salicio <roberto.salicio@gmail.com>
2010-04-18 12:15:33 +0200

    charts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,6 +20,73 @@ $(document).ready(function () {
     selectFirst: false
   }).result(function(e,item){
     location.href = item.url;
-  });                      
+  });
   
+  single_graph();
+  compare_graph();
 });
+
+function single_graph() {
+  var paper = Raphael("holder", 660, 530);
+  
+  var ingr   = ["IMPUESTOS Y TASAS","OTRAS ADMINISTRACIONES","PATRIMONIO","ACTIVOS FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vi = ingresos;
+  var despl = 0;
+  for (var i=0; i < vi.length; i++) {
+    despl = despl + 260*vi[i]/100
+    paper.presus.start_block(0,i*100+i*5,200,100,vi[i],ingr[i],despl,colors[i],305,24);
+  };
+  
+  var ingr   = ["PERSONAL","GASTOS COTIDIANOS","PROYECTOS E INVERSIONES","SUBVENCIONES","OTRAS ENTIDADES","ACTIVOS FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vg = gastos;
+  var despl = 0;
+  for (var i=0; i < vg.length; i++) {
+    despl = despl + 260*vg[i]/100
+    paper.presus.end_block(0,i*70+i*5,200,70,vg[i],ingr[i],despl,colors[i],335,459);
+  };
+  paper.safari();
+};
+
+function compare_graph() {
+  var paper = Raphael("holder2", 460, 530);
+  
+  var ingr   = ["PROYECTOS E INVER.","OTRAS ADMONS.","PATRIMONIO","ACT. FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vi = ingresos;
+  var despl = 0;
+  for (var i=0; i < vi.length; i++) {
+    despl = despl + 260*vi[i]/100
+    paper.presus.start_block(0,i*100+i*5,130,100,vi[i],ingr[i],despl,colors[i],205,20);
+  };
+
+  var ingr   = ["PERSONAL","GASTOS COTIDIANOS","PROYECTOS E INVERSIONES","SUBVENCIONES","OTRAS ENTIDADES","ACTIVOS FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vg = gastos;
+  var despl = 0;
+  for (var i=0; i < vg.length; i++) {
+    despl = despl + 260*vg[i]/100
+    paper.presus.end_block(0,i*70+i*5,130,70,vg[i],ingr[i],despl,colors[i],235,330);
+  };
+  ///////////////////////////////////////////////////////////////////////////////////
+  paper = Raphael("holder3", 460, 530);
+  var ingr   = ["PROYECTOS E INVER.","OTRAS ADMONS.","PATRIMONIO","ACT. FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vi = ingresos2;
+  var despl = 0;
+  for (var i=0; i < vi.length; i++) {
+    despl = despl + 260*vi[i]/100
+    paper.presus.start_block(0,i*100+i*5,130,100,vi[i],ingr[i],despl,colors[i],205,20);
+  };
+
+  var ingr   = ["PERSONAL","GASTOS COTIDIANOS","PROYECTOS E INVERSIONES","SUBVENCIONES","OTRAS ENTIDADES","ACTIVOS FINANCIEROS","PRÉSTAMOS"];
+  var colors = ["#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f","#031e1f"]
+  var vg = gastos2;
+  var despl = 0;
+  for (var i=0; i < vg.length; i++) {
+    despl = despl + 260*vg[i]/100
+    paper.presus.end_block(0,i*70+i*5,130,70,vg[i],ingr[i],despl,colors[i],235,330);
+  };
+  paper.safari();
+};
---
+++ public/javascripts/raphael.presus.js
@@ -0,0 +1,66 @@
+/*
+ * Raphael.flow 0.1 - lospresus implementation, based on Raphaël
+ *
+ * Copyright (c) 2010 Roberto Salicio
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ */
+(function () {
+  Raphael.fn.presus = Raphael.fn.presus || {};
+  Raphael.fn.presus.start_block = function (sx,sy,ex,ey,percent,text,despl,color,start,font_size){
+    var padding = 10;
+    var l = 50;
+
+    var pos = 260*percent/100;
+
+    this.path([
+      "M",sx,sy,
+      "L",sx+ex,sy,
+      "C",(ex+l),sy,(start-l),despl+130-pos,start,despl+130-pos,
+      "L",start+20,despl+130-pos,
+      "L",start+20,despl+130,
+      "L",start,despl+130,
+      "C",(start-l),despl+130,(ex+l),sy+ey,ex,sy+ey,
+      "L",sx,sy+ey,
+      "L",sx+10,sy+ey/2,
+      "z"
+    ]).attr({fill: color, "stroke-width": 0, opacity: percent/100.0+0.35});//.attr({fill: "url(http://www.irunmywebsite.com/images/irunmywebsitesmall.png)", stroke: color});
+
+    var txt = this.print(0, 0, percent+"%", this.getFont("League Gothic"), 60).attr({fill: "#fff"});
+    var bbox = txt.getBBox();
+    txt.translate(sx+ex-bbox.width-padding,sy+bbox.height-5)
+
+    txt = this.print(0, 0, text, this.getFont("League Gothic"), font_size).attr({fill: "#fff"});
+    var bbox = txt.getBBox();
+    txt.translate(sx+ex-bbox.width-padding,sy+bbox.height+(font_size<24 ? 60 : 55));
+  };
+
+  Raphael.fn.presus.end_block = function (sx,sy,ex,ey,percent,text,despl,color,start,sblock){
+    var padding = 10;
+    var l = 50;
+
+    var pos = 260*percent/100;
+
+    this.path([
+      "M",sblock+sx,sy,
+      "L",sblock+sx+ex,sy,
+      "L",sblock+sx+ex-10,sy+ey/2,
+      "L",sblock+sx+ex,sy+ey,
+      "L",sblock+sx,sy+ey,
+      "C",sblock+sx-l,sy+ey,start+20+l,despl+130,start+20,despl+130,
+      "L",start,despl+130,
+      "L",start,despl+130-pos,
+      "L",start+20,despl+130-pos,
+      "C",start+20+l,despl+130-pos,sblock+sx-l,sy,sblock+sx,sy,
+      "z"
+    ]).attr({fill: color, "stroke-width": 0, opacity: percent/100.0+0.35});
+
+    var txt = this.print(0, 0, percent+"%", this.getFont("League Gothic"), 40).attr({fill: "#fff"});
+    var bbox = txt.getBBox();
+    txt.translate(sblock-9+sx+ex-bbox.width-padding,sy+bbox.height-3);
+
+    txt = this.print(0, 0, text, this.getFont("League Gothic"), 20).attr({fill: "#fff"});
+    var bbox = txt.getBBox();
+    txt.translate(sblock-7+sx+ex-bbox.width-padding,sy+bbox.height+37);
+  };
+})();
+
