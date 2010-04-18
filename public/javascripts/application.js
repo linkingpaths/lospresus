9bfb2222a061754a077748c899a9c9cf8073d5b4
Aitor García <aitor.garcia@gmail.com>
2010-04-18 11:06:04 +0200

    Autocomplete js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+  $('#city_search').autocomplete("/search", {
+    dataType: "json",
+    parse: function(data) {
+      return $.map(data, function(row) {
+        return {
+          data: row,
+          value: row.name,
+          result: row.name
+        }
+      });
+    },
+    formatItem: function(item) {
+      return "<a href='"+item.url+"'><span>"+item.name+"</span></a>"
+    },
+    minChars: 2,
+    resultsClass: "suggestedOptions",
+    width: "23em",
+    scroll: false,
+    selectFirst: false
+  }).result(function(e,item){
+    location.href = item.url;
+  });                      
+  
+});
